7a86842c5e187ce198a3471ecc824c53d84063d3
polieftov <polieftovegor@gmail.com>
2021-05-08 11:57:10 +0500

    Red Black Tree

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using CsvHelper;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace LabWork2
+{
+    class Program
+    {
+        static void Test()
+        {
+            //Insertion tests
+            var t1 = new RedBlackTree("a");
+            if (t1.root.value != "a" || t1.root.color != Color.Black)
+                Console.WriteLine("Insert test 1 failed");
+            t1.Add("f");
+            if (t1.root.right.value != "f" || t1.root.right.color != Color.Red)
+                Console.WriteLine("Insert test 2 failed");
+            t1.Add("b");
+            if (t1.root.value != "b" || t1.root.left.value != "a" || t1.root.left.color != Color.Red || t1.root.right.value != "f" || t1.root.right.color != Color.Red)
+                Console.WriteLine("Insert test 3 failed");
+            t1.Add("g");
+            if (t1.root.value != "b" || t1.root.left.value != "a" || t1.root.left.color != Color.Black || t1.root.right.value != "f" || t1.root.right.color != Color.Black || t1.root.right.right.value != "g" ||
+                t1.root.right.right.color != Color.Red)
+                Console.WriteLine("Insert test 4 failed");
+            t1.Add("c");
+            if (t1.root.value != "b" || t1.root.left.value != "a" || t1.root.left.color != Color.Black || t1.root.right.value != "f" || t1.root.right.color != Color.Black || t1.root.right.right.value != "g" ||
+                t1.root.right.right.color != Color.Red || t1.root.right.left.value != "c" || t1.root.right.left.color != Color.Red)
+                Console.WriteLine("Insert test 5 failed");
+            t1.Add("d");
+            if (t1.root.value != "b" || t1.root.left.value != "a" || t1.root.left.color != Color.Black || t1.root.right.value != "f" || t1.root.right.color != Color.Red || t1.root.right.right.value != "g" ||
+                t1.root.right.right.color != Color.Black || t1.root.right.left.value != "c" || t1.root.right.left.color != Color.Black || t1.root.right.left.right.value != "d" || t1.root.right.left.color != Color.Black)
+                Console.WriteLine("Insert test 6 failed");
+            t1.Add("e");
+            if (t1.root.right.left.right.value != "e" || t1.root.right.left.right.color != Color.Red || t1.root.right.left.right.parent.value != "d")
+                Console.WriteLine("Insert test 7 failed");
+            t1.Add("h");
+            if (t1.root.right.right.right.value != "h" || t1.root.right.right.right.color != Color.Red || t1.root.right.right.right.parent.value != "g")
+                Console.WriteLine("Insert test 8 failed");
+
+
+            //Find tests
+            var a = t1.Find("a");
+            if (a.parent.value != "b" || a.value != "a" || a.color != Color.Black)
+                Console.WriteLine("Find test 1 failed");
+            var b = t1.Find("b");
+            if (b != t1.root)
+                Console.WriteLine("Find test 2 failed");
+            var c = t1.Find("z");
+            if (c != null)
+                Console.WriteLine("Find test 3 failed");
+            var d = t1.Find("d");
+            if (d.value != "d" || d.color != Color.Black || d.parent.value != "f" || d.right.value != "e" || d.left.value != "c")
+                Console.WriteLine("Find test 4 failed");
+
+            //Print test
+            var p = t1.PrintTree();
+            if (p != "abcdefgh")
+                Console.WriteLine("Print test failed");
+            //ToList test
+            var l = string.Join("", t1.ToList().Select(x => x.value));
+            if (l != "abcdefgh")
+                Console.WriteLine("ToList test failed");
+        }
+
+        static void Main()
+        {
+            Test();
+
+            
+            //Experiment
+            var minElemLen = 0;
+            var maxElemLen = 20;
+            var minLen = 0;
+            var maxLen = 100;
+            var repeat = 1;
+            var diff = 1;
+            var xDoc = new XmlDocument();
+            xDoc.Load("exp.xml");
+            var xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xNode in xRoot)
+            {
+
+                if (xNode.Name == "minElement")
+                    minElemLen = Int32.Parse(xNode.InnerText);
+
+                if (xNode.Name == "maxElement")
+                    maxElemLen = Int32.Parse(xNode.InnerText);
+
+                if (xNode.Name == "startLength")
+                    minLen = Int32.Parse(xNode.InnerText);
+                
+                if (xNode.Name == "diff")
+                    diff = Int32.Parse(xNode.InnerText);
+
+                if (xNode.Name == "maxLength")
+                    maxLen = Int32.Parse(xNode.InnerText);
+
+                if (xNode.Name == "repeat")
+                    repeat = Int32.Parse(xNode.InnerText);
+            }
+
+            using (var writer = new StreamWriter("res.csv",false))
+            using (var csv = new CsvWriter(writer, new CsvHelper.Configuration.CsvConfiguration(CultureInfo.CurrentCulture) { Delimiter = "\\" }))
+            {
+                
+                for (var size = minLen; size < maxLen; size += diff)
+                {
+                    var rbTree = new RedBlackTree();
+                    var arr = MakeRandStrToArr(size, minElemLen, maxElemLen);
+                    foreach (var e in arr)
+                        rbTree.Add(e);
+                    csv.WriteRecord(size);
+                    csv.WriteRecord(rbTree.GetIerations());//надо как-то подсчитать итерации, затраченные на это
+                    csv.NextRecord();
+                }
+                writer.Flush();
+            }
+            Console.WriteLine();
+        }
+
+        private static string[] MakeRandStrToArr(int size, int minElemLen, int maxElemLen)
+        {
+            var arr = new string[size];
+            for (var i = 0; i < size; i++)
+                arr[i] = MakeRndStr(minElemLen, maxElemLen);
+            return arr;
+        }
+
+        private static string MakeRndStr(int minElemLen, int maxElemLen)
+        {
+            var rand = new Random();
+            var l = rand.Next(minElemLen, maxElemLen);
+            var builder = new StringBuilder(l);
+            for (var i = 0; i < l; i++)
+                builder.Append((char)rand.Next('a', 'z'));
+            return builder.ToString();
+        }
+    }
+}
---
+++ RedBlackTree.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabWork2
+{
+    public enum Color
+    {
+        Red = 0,
+        Black = 1
+    }
+
+    class RBNode
+    {
+        public RBNode(string val)
+        {
+            value = val;
+            count = 0;
+        }
+
+        public RBNode(string val, RBNode par)
+        {
+            parent = par;
+            value = val;
+        }
+
+        
+        public string value;
+        public Color color = Color.Red;
+        public RBNode parent;
+        public RBNode left;
+        public RBNode right;
+        public int count;//кол-во элементов с этим значением
+    }
+
+    class RedBlackTree
+    {
+        private int iterations;
+        public RBNode root;
+        public RedBlackTree()
+        {
+            root = null;
+        }
+        public RedBlackTree(string r)
+        {
+            root = new RBNode(r);
+            root.color = Color.Black;
+        }
+
+        public int GetIerations()
+        {
+            var res = iterations;
+            iterations = 0;
+            return res;
+        }
+
+        public RBNode Find(string v)
+        {
+            var x = root;
+            while(x != null && x.value != v)
+            {
+                iterations++;
+                if (String.Compare(x.value, v) < 0)
+                    x = x.right;
+                else
+                    x = x.left;
+            }
+            return x;
+        }
+
+        public List<RBNode> ToList()
+        {
+            var res = new List<RBNode>();
+            AddNode(root);
+
+            void AddNode(RBNode n)
+            {
+                if (n.left != null)
+                    AddNode(n.left);
+
+                res.Add(n);
+
+                if (n.right != null)
+                    AddNode(n.right);
+            }
+            return res;
+        }
+
+        public string PrintTree()
+        {
+            var res = "";
+            PrintNode(root);
+
+            void PrintNode(RBNode n)
+            {
+                if (n.left != null)
+                    PrintNode(n.left);
+
+                res += n.value;
+
+                if (n.right != null)
+                    PrintNode(n.right);
+            }
+            return res;
+        }
+
+        protected RBNode AddNode(RBNode to, string data)
+        {
+            iterations++;
+            if (data == to.value)
+            {
+                to.count++;
+                return to;
+            }
+
+            if (String.Compare(data, to.value) < 0) //добавляем влево
+            {
+                if (to.left != null)
+                    return AddNode(to.left, data);
+                return to.left = new RBNode(data, to);
+            }
+            else //добавляем вправо
+            {
+                if (to.right != null)
+                    return AddNode(to.right, data);
+                return to.right = new RBNode(data, to);
+            }          
+        }
+
+        //добавление элемента с соблюд кч деревьев
+        public RBNode Add(string data)
+        {
+            
+            if (root == null)
+            {
+                root = new RBNode(data);
+                root.color = Color.Black;
+                return root;
+            }
+            var n = AddNode(root, data);
+
+            //нормализация дерева
+            
+            Insert(n);
+            root.color = Color.Black;
+            return n;
+        }
+
+        private void Insert(RBNode x)
+        {
+            var y = new RBNode("");
+            x.color = Color.Red;
+            while (x != root && x.parent.color == Color.Red)
+            {
+                if (x.parent == x.parent.parent.left)
+                {
+                    y = x.parent.parent.right == null? new RBNode("", x.parent.parent.right) { color = Color.Black } : x.parent.parent.right;
+                    if (y.color == Color.Red)
+                    {
+                        x.parent.color = Color.Black;
+                        y.color = Color.Black;
+                        x.parent.parent.color = Color.Red;
+                        x = x.parent.parent;
+                    }
+                    else
+                    {
+                        if (x == x.parent.right)
+                        {
+                            x = x.parent;
+                            rotate_left(x);
+                        }
+                        
+                        x.parent.color = Color.Black;
+                        x.parent.parent.color = Color.Red;
+                        rotate_right(x.parent.parent);
+                    }
+                    
+                }
+                //
+                else
+                {
+                    y = x.parent.parent.left == null? new RBNode("", x.parent.parent.left) { color = Color.Black } : x.parent.parent.left;
+                    if (y.color == Color.Red)
+                    {
+                        x.parent.color = Color.Black;
+                        y.color = Color.Black;
+                        x.parent.parent.color = Color.Red;
+                        x = x.parent.parent;
+                    }
+                    else{
+                        if (x == x.parent.left)
+                        {
+                            x = x.parent;
+                            rotate_right(x);
+                        }
+                    
+                        
+                        x.parent.color = Color.Black;
+                        x.parent.parent.color = Color.Red;
+                        rotate_left(x.parent.parent);
+                    }
+
+                }
+            }
+        }
+
+        //повоторот вправо
+
+        protected void rotate_right(RBNode n)
+        {
+            
+            var y = new RBNode("");
+            if (n.left != null)
+            {
+                y = n.left;
+                n.left = y.right;
+
+
+
+                if (y.right != null)
+                    y.right.parent = n;
+                y.parent = n.parent;
+                if (n.parent == null)
+                    root = y;
+                else if (n == n.parent.right)
+                    n.parent.right = y;
+                else
+                    n.parent.left = y;
+                y.right = n;
+                n.parent = y;
+            }
+            else
+            {
+                y = n.parent;
+                y.parent.right = n;
+                n.parent = n;
+                n.parent.right = y;
+                n.parent = y.parent;
+                y.parent = n;
+                y.left = null;
+            }
+
+         
+        }
+        //поворот влево
+
+        protected void rotate_left(RBNode n)
+        {
+
+            var y = new RBNode("");
+            if (n.right != null)
+            {
+                y = n.right;
+                n.right = y.left;
+
+                if (y.left != null)
+                    y.left.parent = n;
+                y.parent = n.parent;
+                if (n.parent == null)
+                    root = y;
+                else if (n == n.parent.left)
+                    n.parent.left = y;
+                else
+                    n.parent.right = y;
+                y.left = n;
+                n.parent = y;
+            }
+            else
+            {
+                y = n.parent;
+
+                y.parent.left = n;
+                n.parent = n;
+                n.parent.left = y;
+                n.parent = y.parent;
+                y.parent = n;
+                y.right = null;
+            }
+            ////if (n.parent == null)
+            ////    return;
+
+            //////левый потомок узла ставим в правый родительского
+            ////if (n.left != null)
+            ////{
+            ////    n.parent.right = n.left;
+            ////    n.parent.right.parent = n.parent;
+            ////}
+            ////else
+            ////    n.parent.right = null;
+
+            ////n.left = n.parent;//родительский ставим на его место
+            ////n.parent = n.parent.parent;//ссылку на нового родителя берем у бывшего родителя
+
+            ////n.left.parent = n;//бывшему родителю ставим ссылку на текущий узел
+
+            ////if (n.parent != null)//если нужно поправить ссылку у дедушки
+            ////{
+            ////    if (n.parent.left == n.left)
+            ////        n.parent.left = n;
+            ////    if (n.parent.right == n.left)
+            ////        n.parent.right = n;
+            ////}
+            ////else //корень дерева поменялся
+            ////    this.root = n;
+
+        }
+
+
+
+        //найти дедушку
+
+        protected RBNode grandparent(RBNode n)
+        {
+            if ((n != null) && (n.parent != null))
+                return n.parent.parent;
+            else
+                return null;
+        }
+
+        //найти дядю
+
+        protected RBNode uncle(RBNode n)
+        {
+            RBNode g = grandparent(n);
+            if (g == null)
+                return null;
+            if (n.parent == g.left)
+                return g.right;
+            else
+                return g.left;
+
+        }
+      
+    }
+    
+}
